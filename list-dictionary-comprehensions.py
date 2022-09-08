b63fa423ced2a2a024491257236cc28a4444c2ac
Marat Ayaibergenov <marataya@gmail.com>
2022-09-09 03:10:10 +0600

    list-dictionary-comprehensions.py

--- list-dictionary-comprehensions.py
+++ list-dictionary-comprehensions.py
@@ -3,7 +3,9 @@ from typing_extensions import LiteralString
 
 list = list(range(100))
 
-print([5*item for item in list if item % 2 == 0])
+list1 = [5*item for item in list if item % 2 == 0]
+
+print(list1)
 
 string = "Bring and place it on the table. Do not touch anything."
 
@@ -17,10 +19,19 @@ print([cleanWords(word) for word in string.split() if len(cleanWords(word))>2])
 
 print(2**9 / 1.4)
 
-animalList = [('a','aardvark'),('b','bear'),('c','cat')]
+animalList = [('a','aardvark'),('b','bear'),('c',['cat','coyote'])]
 animals = {item[0]: item[1] for item in animalList}
 print(animals)
 
 # unpacking tuples into key, value vars
 animals = {key: value for key, value in animalList}
 print(animals)
+print(type(animals.items()))
+print(animals.items())
+print([{'letter': key, "name": value} for key,value in animals.items()])
+
+dishes = {'eggs': 2, 'sausage': 1, 'bacon': 1, 'spam': 500}
+keys = dishes.keys()
+values = dishes.values()
+
+print(type(range(10)))
